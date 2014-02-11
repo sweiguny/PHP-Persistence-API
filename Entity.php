e4f95d9be46cf553c79daacaf94f57efe4b052cb
sweiguny <s.weiguny@gmail.com>
2014-02-11 21:56:45 +0100

    OneToOne(eager) implemented - refactoring needed

--- Entity.php
+++ Entity.php
@@ -8,8 +8,7 @@ namespace PPA;
  */
 class Entity {
 
-    
-
+   
 //    public function __get($name) {
 //        if (!is_object($name) && isRelation($name)) {
 //            new sql\Query("select * from <tablename> where <idcol> = <val>");
