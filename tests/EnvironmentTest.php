fa0569bccc68b3b2cbbb501386da65eaeb97e476
sweiguny <s.weiguny@gmail.com>
2017-07-27 12:01:55 +0200

    added travis and phpunit

---
+++ tests/EnvironmentTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PPA\tests;
+
+use PHPUnit\Framework\TestCase;
+
+class EnvironmentTest extends TestCase
+{
+    
+    public function testOne()
+    {
+        $this->assertTrue(true);
+    }
+    
+}
+
+?>
