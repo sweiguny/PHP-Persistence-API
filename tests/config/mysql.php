1bb7fbf867e896a2bd557bf095582710742b4870
sweiguny <s.weiguny@gmail.com>
2019-03-21 16:03:36 +0100

    travis adaptions, take 3

--- tests/config/mysql.php
+++ tests/config/mysql.php
@@ -3,5 +3,5 @@
     "hostname" => "127.0.0.1",
     "database" => "ppa_test",
     "username" => "root",
-    "password" => "lei6395derf"
+    "password" => ""
 ];
--- tests/config/pgsql.php
+++ tests/config/pgsql.php
@@ -3,5 +3,5 @@
     "hostname" => "127.0.0.1",
     "database" => "ppa_test",
     "username" => "postgres",
-    "password" => "lei6395derf"
+    "password" => ""
 ];
